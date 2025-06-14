4f325c5e063ffae3b6da4d1efeeb259ead5826bb
vinod <vinod@gmail.com>
2025-06-14 09:17:49 +0530

    Button updation on project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 export const routes: Routes = [
   { path: '',component: HomeComponent,title: 'Home' },
+  { path: 'todos',component: TodosComponent,title: 'Todos' }
 ];
+
+    @NgModule({
+       imports: [RouterModule.forRoot(routes)],
+       exports: [RouterModule]
+     })
+     export class AppRouting { }
