9bbee6bc4929ac2ec5cd577c2e1448c3ef9979f2
H Chekresh <hosbectchekresh@gmail.com>
2025-06-15 08:18:18 +0530

    small changes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { routes } from '../Practice/src/app/app.routes';
+import { routes } from './app/app.routes';
 import { provideRouter } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
