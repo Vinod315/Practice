c6bf617ebae085cdd85c93906863b76e1eeef68b
H Chekresh <hosbectchekresh@gmail.com>
2025-06-15 08:26:19 +0530

    variable assignment changed

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -12,22 +12,19 @@ export class TodosComponent {
    S=" ";
 
   addClicked() {
-    this.S=" ";
     console.log("Add Button clicked");
-    this.S+="Add Button clicked";
+    this.S="Add Button clicked";
 
   }
 
   deleteClicked() {
-    this.S=" ";
     console.log("Delete Button clicked");
-    this.S+="Delete Button clicked";
+    this.S="Delete Button clicked";
   }
 
   updateClicked() {
-    this.S=" ";
     console.log("Update  Button clicked");
-    this.S+="Update Button clicked";
+    this.S="Update Button clicked";
   }
 
 
