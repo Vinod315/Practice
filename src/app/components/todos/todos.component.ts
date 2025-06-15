0de6ebddccb57eec15c2fe909ef9866c42afc2e0
vinod <vinod@gmail.com>
2025-06-15 07:21:57 +0530

    Button status updation

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -9,8 +9,27 @@ import { Component } from '@angular/core';
 
 
 export class TodosComponent {
+   S=" ";
+
   addClicked() {
+    this.S=" ";
     console.log("Add Button clicked");
+    this.S+="Add Button clicked";
+
   }
+
+  deleteClicked() {
+    this.S=" ";
+    console.log("Delete Button clicked");
+    this.S+="Delete Button clicked";
+  }
+
+  updateClicked() {
+    this.S=" ";
+    console.log("Update  Button clicked");
+    this.S+="Update Button clicked";
+  }
+
+
 }
 
