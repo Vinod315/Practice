bfccb9b92351655b3989469094e6d8c1713eaf1b
H Chekresh <hosbectchekresh@gmail.com>
2025-06-20 22:00:23 +0530

    Add navigation links to the top bar and update routing for Settings component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { TodosComponent } from './components/todos/todos.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home' },
-  { path: 'todos', component: TodosComponent, title: 'Todos' }
+  { path: 'todos', component: TodosComponent, title: 'Todos' },
+  { path: 'settings', component: SettingsComponent, title: 'Settings' }
 ];
 
 @NgModule({
