a9873c664a47349a84a7c59055e7bc93958c3da6
H Chekresh <hosbectchekresh@gmail.com>
2025-06-22 11:23:52 +0530

    Update Settings component to reflect selected week start day and improve styling

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class SettingsComponent {
 
+  ischeckedvalue: string = '';
+
+  onSelectChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.ischeckedvalue = selectElement.value;
+    console.log(this.ischeckedvalue);
+    if(this.ischeckedvalue === 'Monday'){
+      this.ischeckedvalue = 'Monday';
+    }
+    else if(this.ischeckedvalue === 'Tuesday'){
+      this.ischeckedvalue = 'Tuesday';
+    }
+    else if(this.ischeckedvalue === 'Wednesday'){
+      this.ischeckedvalue = 'Wednesday';
+    }
+    else if(this.ischeckedvalue === 'Thursday'){
+      this.ischeckedvalue = 'Thursday';
+    }
+    else if(this.ischeckedvalue === 'Friday'){
+      this.ischeckedvalue = 'Friday';
+    }
+    else if(this.ischeckedvalue === 'Saturday'){
+      this.ischeckedvalue = 'Saturday';
+    }
+    else if(this.ischeckedvalue === 'Sunday'){
+      this.ischeckedvalue = 'Sunday';
+    }
+    else{
+      this.ischeckedvalue = 'System Default';
+    }
+  }
+
 }
